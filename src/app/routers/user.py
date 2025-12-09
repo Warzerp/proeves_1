85d530fc26495c5a8402e94756e41faecda781e4
Warzerp <jhonthegamer2@gmail.com>
2025-12-08 23:38:18 -0500

    Refactor user router for improved clarity

--- src/app/routers/user.py
+++ src/app/routers/user.py
@@ -198,4 +198,5 @@ def deactivate_user(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error al desactivar usuario: {str(e)}"
+
         )
